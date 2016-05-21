0f52b18f7b12f3e97e2de72b817b7aa7f6c0319b
Ron <ron@rafrancis.com>
2016-05-21 23:56:30 +0100

    Add project files.

---
+++ LINQtoSQLStoredProc/ClsCustomer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data.Linq.Mapping;
+using System.Data.Linq;
+using System.Reflection;
+
+namespace LINQtoSQLStoredProc
+{
+    [Table(Name = "Customer")]
+    public class clsCustomerEntity
+    {
+        private int _CustomerId;
+        private string _CustomerCode;
+        private string _CustomerName;
+
+        [Column(DbType = "nvarchar(50)")]
+        public string CustomerCode
+        {
+            set
+            {
+                _CustomerCode = value;
+            }
+            get
+            {
+                return _CustomerCode;
+            }
+        }
+
+        [Column(DbType = "nvarchar(50)")]
+        public string CustomerName
+        {
+            set
+            {
+                _CustomerName = value;
+            }
+            get
+            {
+                return _CustomerName;
+            }
+        }
+
+        [Column(DbType = "int", IsPrimaryKey = true)]
+        public int CustomerId
+        {
+            set
+            {
+                _CustomerId = value;
+            }
+            get
+            {
+                return _CustomerId;
+            }
+        }
+    }
+
+    public class clsMyContext : DataContext
+    {
+        public clsMyContext(string connection)
+            : base(connection)
+        {
+        }
+        [Function(Name = "usp_SelectCustomer", IsComposable = false)]
+        public ISingleResult<clsCustomerEntity> getCustomerAll()
+        {
+            
+            IExecuteResult objResult = this.ExecuteMethodCall(this,(MethodInfo)(MethodInfo.GetCurrentMethod()));
+            ISingleResult<clsCustomerEntity> objresults = (ISingleResult<clsCustomerEntity>) objResult.ReturnValue;
+            return objresults;
+        }
+    }
+}
---
+++ LINQtoSQLStoredProc/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace LINQtoSQLStoredProc
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        //string strConnectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\Sales.mdf;Integrated Security=True;User Instance=True";
+        //string strConnectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\Sales.mdf;Integrated Security=True;User Instance=True";
+        string strConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Sales.mdf;Integrated Security=True";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            clsMyContext objContext = new clsMyContext(strConnectionString);
+            foreach(var row in objContext.getCustomerAll())
+            {
+                Response.Write(row.CustomerCode);
+            }
+        }
+    }
+}
